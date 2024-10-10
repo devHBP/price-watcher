2c55d1047a7c2a7b1975d701ad135e8b460320ec
Développeur HBP <hbp@macbookppeurhbp.home>
2024-10-10 09:10:29 +0200

    Sortie du middleware de la route post du scraper

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,10 @@ Route::middleware('auth')->group(function(){
         Artisan::call('app:check-and-update-historique-prix');
         return response()->json(['message' => 'Prix synchronisés.']);
     })->name('sync-prix');
-
-    // Route déclenchée par le script python
-    Route::post('/services/controle-prix', [ServicesController::class, 'controleAlertePrix']);
-
 });
+
+// Route déclenchée par le script python, sans middleware.
+Route::post('/services/controle-prix', [ServicesController::class, 'controleAlertePrix']);
 
 Route::middleware('auth')->group(function(){
     // Route CRUD Produits, Categorie, Concurrents
